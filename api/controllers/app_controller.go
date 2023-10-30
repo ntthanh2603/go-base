b5a5de8823aaefdd629dc4faf111209967c12e39
Quang Vinh <l1ttps443@gmail.com>
2023-10-30 18:41:38 +0700

    refactor: import guard

--- api/controllers/app_controller.go
+++ api/controllers/app_controller.go
@@ -7,20 +7,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	UseGuard  = guard.UseGuard
+	TestGuard = guard.TestGuard
+)
+
 // AppController is a function that handles the routing for the root URL ("/").
 //
 // It takes in a *gin.RouterGroup parameter named "r" which represents the router group that the function will be added to.
 //
 // There are no return types specified for this function.
 func AppController() *gin.Engine {
+
 	appService := services.AppService()
 	return Controller("/hello-world",
 		Get("/",
 			func() any {
-
 				return appService.HelloWorldGet()
 			},
-			guard.UseGuard(guard.TestGuard),
+			UseGuard(TestGuard),
 		),
 	)
 }
